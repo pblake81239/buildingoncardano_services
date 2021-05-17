ee63cf63347e399f2391ff0a429c518e6d9c5493
pblake81239 <paddyblake09@hotmail.com>
2021-05-17 19:50:04 +0100

    Add files via upload

---
+++ src/main/java/com/buildingon/cardano/boc/BocApplication.java
@@ -0,0 +1,13 @@
+package com.buildingon.cardano.boc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildingon/cardano/boc/BocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildingon.cardano.boc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
