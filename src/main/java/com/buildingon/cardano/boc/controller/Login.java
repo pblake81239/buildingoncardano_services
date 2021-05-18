56d7908ace26df4b882b32cc8ccd9b8f0c3a7ce9
paddy <paddyblake09@hotmail.com>
2021-05-18 22:43:19 +0100

    Add login

--- src/main/java/com/buildingon/cardano/boc/controller/Login.java
+++ src/main/java/com/buildingon/cardano/boc/controller/Login.java
@@ -1,10 +1,37 @@
 package com.buildingon.cardano.boc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.buildingon.cardano.boc.dto.User;
+import com.buildingon.cardano.boc.repo.UserRepository;
+
+@RestController
+@RequestMapping("/login")
 public class Login {
 
+	@Autowired
+	UserRepository userRepo;
 
-	
+	@PostMapping("/user")
+	public String processRegister(@RequestBody User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		User dbUser = userRepo.findByEmail(user.getEmail());
+
+		if (dbUser != null) {
+			if (passwordEncoder.matches(user.getPassword(), dbUser.getPassword())) {
+				return "valid_user";
+			} else {
+				return "not_valid";
+			}
+		} else {
+			return "User doesnt exist";
+		}
+	}
 }
