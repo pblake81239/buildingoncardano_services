87bf64ae5ac8da1a87c60e980a1a74e837179500
paddy <paddyblake09@hotmail.com>
2021-05-20 20:57:58 +0100

    update

--- startService.sh
+++ startService.sh
@@ -3,4 +3,4 @@ git pull
 
 mvn install
 
-nohup java -Xms1024m -Xmx2048m -jar /home/shamrock/git/buildingoncardano_services/boc-0.0.1-SNAPSHOT.jar &
+nohup java -Xms1024m -Xmx2048m -jar /home/shamrock/git/buildingoncardano_services/target/boc-0.0.1-SNAPSHOT.jar &
