b66fedb944ae163caea74053c11f471bf51ba285
paddy <paddyblake09@hotmail.com>
2021-06-23 12:06:29 +0100

    only verified apps

--- src/main/java/com/buildingon/cardano/boc/repo/ProjectSalesRepository.java
+++ src/main/java/com/buildingon/cardano/boc/repo/ProjectSalesRepository.java
@@ -14,7 +14,9 @@ public interface ProjectSalesRepository extends JpaRepository<ProjectSales, Long
     @Query("SELECT p FROM ProjectSales p where p.saleStatus = 'Live'")
     public List<ProjectSales> getLiveSalesOfAllProjects();
     
-    @Query("SELECT p FROM ProjectSales p where p.saleStatus = 'Live' OR p.saleStatus = 'Upcoming' order by p.saleStartDate")
+    @Query("SELECT p FROM ProjectSales p where p.saleStatus = 'Live' OR p.saleStatus = 'Upcoming' "
+    		+ "and p.projectName in (select pj.name from Project pj where pj.verified='true')"
+    		+ "order by p.saleStartDate")
     public List<ProjectSales> getLiveAndUpcomingSalesOfAllProjects();
 
 }
